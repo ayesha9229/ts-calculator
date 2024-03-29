8cd59d83e5cac828e60cb6c2b9d764d6f7070db4
Kaneez Ayesha <kaneezayesha9229@gmail.com>
2024-03-30 04:47:57 +0500

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+let validationFunc = (input) => !isNaN(parseFloat(input));
+let value = await inquirer.prompt([
+    {
+        name: "firstValue",
+        message: "Enter first value: ",
+        type: "number",
+        validate: async (input) => validationFunc(input),
+    },
+    {
+        name: "secondValue",
+        message: "Enter second value: ",
+        type: "number",
+        validate: async (input) => validationFunc(input),
+    },
+    {
+        name: "operator",
+        message: "Select Operator: ",
+        type: "list",
+        choices: ['+', '-', '*', '/']
+    }
+]);
+let result;
+if (value.operator === '+') {
+    result = value.firstValue + value.secondValue;
+}
+else if (value.operator === '-') {
+    result = value.firstValue - value.secondValue;
+}
+else if (value.operator === '*') {
+    result = value.firstValue * value.secondValue;
+}
+else {
+    result = value.firstValue / value.secondValue;
+}
+console.log(`Result is ${result}`);
